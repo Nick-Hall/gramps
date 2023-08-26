8645f72cfddd56d80c1e8f86b9096cbe97a6813f
Nick Hall <nick-h@gramps-project.org>
2023-08-26 22:57:50 +0100

    wip

--- aio/build.sh
+++ aio/build.sh
@@ -1,3 +1,8 @@
+# Parameters: $1=Version $2=Build
+
+AIO=`pwd`
+echo $AIO
+
 # install prerequisites
 ## prerequisites in msys packages
 pacman -S --needed --noconfirm mingw-w64-x86_64-python mingw-w64-x86_64-python-pip mingw-w64-x86_64-gexiv2 mingw-w64-x86_64-ghostscript mingw-w64-x86_64-python-cairo mingw-w64-x86_64-python-gobject mingw-w64-x86_64-python-icu mingw-w64-x86_64-iso-codes mingw-w64-x86_64-hunspell mingw-w64-x86_64-enchant perl-XML-Parser intltool mingw-w64-x86_64-python-lxml mingw-w64-x86_64-python-jsonschema mingw-w64-x86_64-gspell mingw-w64-x86_64-geocode-glib mingw-w64-x86_64-python-pillow git mingw-w64-x86_64-graphviz mingw-w64-x86_64-goocanvas mingw-w64-x86_64-osm-gps-map base-devel subversion mingw-w64-x86_64-python-graphviz mingw-w64-x86_64-osm-gps-map mingw-w64-x86_64-nsis mingw-w64-x86_64-python-cx-freeze  mingw-w64-x86_64-python-requests mingw-w64-x86_64-adwaita-icon-theme mingw-w64-x86_64-python-networkx mingw-w64-x86_64-python-psycopg2 upx mingw-w64-x86_64-python-packaging unzip mingw-w64-x86_64-python-nose mingw-w64-x86_64-python-wheel
@@ -64,7 +69,7 @@ cd /mingw64/share/enchant/voikko
 wget https://www.puimula.org/htp/testing/voikko-snapshot-v5/dict.zip
 unzip dict.zip
 
-cd D:/a/gramps/gramps/aio
+cd $AIO
 
 ## create a directory structure for icons
 mkdir /mingw64/share/icons/gnome
@@ -92,13 +97,11 @@ cp /mingw64/share/icons/hicolor/scalable/places/*.svg /mingw64/share/icons/gnome
 # build gramps
 rm -rf dist aio/dist
 python setup.py bdist_wheel
-appbuild="r$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)"
-appversion=$(grep "^VERSION_TUPLE" gramps/version.py|sed 's/.*(//;s/, */\./g;s/).*//')
 unzip -d aio/dist dist/*.whl
 cd aio
 
 # create nsis script
-cat grampsaio64.nsi.template|sed "s/yourVersion/$appversion/;s/yourBuild/$appbuild/">grampsaio64.nsi
+cat grampsaio64.nsi.template|sed "s/yourVersion/$1/;s/yourBuild/$2/">grampsaio64.nsi
 # build cx_freeze executables
 python setup.py build_exe --no-compress
 # build installer
